c57943c921c208de41b111d5eecf69e29699d74d
Patryk <patrykr124@gmail.com>
2025-02-27 13:09:47 +0100

    modified:   frontend/src/lib/axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const baseURL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:5000/api/"
-    : "https://message-socket-io-appserver.vercel.app/";
+    : "https://message-socket-io-appfrontend-pz8cqvt35.vercel.app/";
 
 
 export const axiosInstance = axios.create({
