602d2da68465d5f5c6a4f99d46b90515cc17a40b
Patryk <patrykr124@gmail.com>
2025-02-27 13:35:46 +0100

    modified:   frontend/src/lib/axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,13 +1,6 @@
 import axios from "axios";
 
-const baseURL =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:5000/api/"
-    : "https://message-socket-io-appserver.vercel.app/api/";
-
-
 export const axiosInstance = axios.create({
-  baseURL,
+  baseURL: "http://localhost:5000/api/",
   withCredentials: true,
-  
 });
